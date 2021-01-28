3874a0912dc6cca06df1a86376ceb87e8c4a5ea6
Roy <rdg01834@gmail.com>
2021-01-28 23:37:44 +0600

    data types problem have to solve

---
+++ 10 dayd of js/data-types.js
@@ -0,0 +1,28 @@
+function performOperation(secondInteger, secondDecimal, secondString) {
+    const firstInteger = 4;
+    const secondInteger = 12;
+    
+    const firstDecimal = 4.0;
+    const secondDecimal = 4.32;
+    
+    const firstString = 'HackerRank ';
+    const secondString = 'is the best place to learn and practice coding!';
+    
+    console.log(firstInteger + secondInteger);
+    
+    console.log(firstDecimal + secondDecimal);
+    
+    console.log(firstString + ' ' + secondString);
+
+    // have to solve this
+ 
+
+}
+
+function main() {
+    const secondInteger = readLine();
+    const secondDecimal = readLine();
+    const secondString = readLine();
+    
+    performOperation(secondInteger, secondDecimal, secondString);
+}
