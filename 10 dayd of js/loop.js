dc45206299d958c5bdd386d7b1e85e135222944b
Roy <rdg01834@gmail.com>
2021-07-16 00:15:13 +0600

    simple arry sum problem solved

--- 10 dayd of js/loop.js
+++ 10 dayd of js/loop.js
@@ -71,7 +71,22 @@ Then the consonants are printed in the same order as they appeared in .
  */
 function vowelsAndConsonants(s) {
     
-    //need to solve it
+    var vowels = ['a', 'e', 'i', 'o', 'u']
+    for (var i = 0; i < s.length; i++)
+        {
+            if (vowels.indexOf(s[i]) > -1)
+                {
+                    console.log(s[i]);
+                }
+        }
+    
+    for (var j = 0; j < s.length; j++)
+        {
+            if (vowels.indexOf(s[j]) < 0)
+                {
+                    console.log(s[j]);
+                }
+        }
 }
 
 
